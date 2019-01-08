b450361fb6e0809d0c1bdba01bf5964153cdeb70
pengkun <pengkun@jingzhengu.com>
2019-01-08 17:05:14 +0800

    合并分享库到此lib

--- android/src/main/java/com/timson/react_native_wx_ali_pay/RNWxAliPayPackage.java
+++ android/src/main/java/com/timson/react_native_wx_ali_pay/RNWxAliPayPackage.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.timson.react_native_wx_ali_pay.share.WeChatModule;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,16 +20,9 @@ public class RNWxAliPayPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new RNWxAliPayModule(reactContext));
+        modules.add(new WeChatModule(reactContext));
         return modules;
     }
-
-    // 已弃用
-    // 在react-native@0.50.0 已经开始报错
-    // @Override
-    // public List<Class<? extends JavaScriptModule>> createJSModules() {
-    //     return Collections.emptyList();
-    // }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
